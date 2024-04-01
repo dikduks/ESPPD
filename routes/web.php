f0776c533efa48c0476b66fecfb0725d55a190c3
dikduks <dandika.kristanto@gmail.com>
2024-04-01 14:00:11 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\PegawaiController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/', [PegawaiController::class, 'index']);
+Route::get('/pegawai',[PegawaiController::class,'create']);
+// Route::get('/',[PegawaiController::class, 'create']);
