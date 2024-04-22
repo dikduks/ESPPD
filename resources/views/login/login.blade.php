8dbf2e3a82d880e5036d086ac8438351e807acc1
dikduks <dandika.kristanto@gmail.com>
2024-04-22 15:37:05 +0700

    au akh

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,3 +1,13 @@
-<?PHP
-echo "Selamat Datang Administrator";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/pegawai"> Klik disini untuk kembali </a>
+</body>
+</html>
+ 
