b9e6e3dfbfb51314e490970e758c150b7eb77d8d
dika Laptop <dandika@poltekkes-malang.ac.id>
2024-04-26 09:17:26 +0700

    operator panah

---
+++ resources/views/operatorPanah.blade.php
@@ -0,0 +1,10 @@
+<?php
+// Membuat instance baru dari kelas User
+$user = new User;
+
+// Mengakses properti 'name' dari objek $user
+echo $user->name;
+
+// Memanggil metode 'save' pada objek $user
+$user->save();
+?>
